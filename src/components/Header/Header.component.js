fe2227fcb45ffba743bbcbf2b13e7fbf3b129c48
Spiritusanti <mccrackenjacob@gmail.com>
2021-05-28 13:33:28 -0700

    modified header component so that website title routes use directly to homepage when clicked

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "./Header.styles.scss";
 
 const Header = () => {
   return (
     <div className="container">
       <div className="title-container">
-        <h1>Get shit done!</h1>
+        <h1><Link to="/">Get shit done!</Link></h1>
         <span>A not so average To Do App</span>
       </div>
     </div>
