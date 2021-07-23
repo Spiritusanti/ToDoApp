9eecb6a265435a10c11df0bd01d3ec5cff69b62d
Spiritusanti <mccrackenjacob@gmail.com>
2021-07-23 10:52:27 -0700

    added in firebaseui again plus config

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+import firebase from "../../firebase/firebase";
 import { StyledFirebaseAuth } from "react-firebaseui";
 
 const uiConfig = {
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,5 @@
 import firebase from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-};
+import firebaseConfig from '../config/firebaseConfig';
 
 firebase.initializeApp(firebaseConfig);
 
